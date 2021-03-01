b44db10c5dbd343bfff18ba9499b4fc688ac17ca
FernanEd <fernandoed.bm@gmail.com>
2021-03-01 12:29:03 -0600

    initial

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # mapache-coffee
+
 web shop for a coffe business
---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import './App.css';
+
+function App() {
+  let msg = 'Hola';
+
+  return <main className="bg-red-700">{msg}</main>;
+}
+
+export default App;
